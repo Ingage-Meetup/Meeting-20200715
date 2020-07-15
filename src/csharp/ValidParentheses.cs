5207d9a50099cfc5b51cc33d6cd943479a3c0462
Ken Baum <ilpadre1953@gmail.com>
2020-07-15 19:53:25 -0400

    Update ValidParentheses.cs

--- src/csharp/ValidParentheses.cs
+++ src/csharp/ValidParentheses.cs
@@ -34,32 +34,7 @@ namespace ValidParentheses
 
         public static bool ValidParentheses(string braces)
         {
-            int left = 0, right = 0;
             bool valid = true;
-            for (int i = 0; i < braces.Length; i++)
-            {
-                switch (braces[i])
-                {
-                    case '(':
-                        left++;
-                        break;
-                    case ')':
-                        right++;
-                        break;
-                    default:
-                        System.Console.WriteLine($"Invalid input string: {braces}");
-                        break;
-                }
-                if (right > left)
-                {
-                    valid = false;
-                    break;
-                }
-            }
-            if (left != right)
-            {
-                valid = false;
-            }
             return valid;
         }
     }
